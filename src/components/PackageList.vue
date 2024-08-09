fb025dc42eb7893e473e1cb3cf4d0695031204b3
Roman Zhekoldin <zhekoldin@bk.ru>
2024-08-09 19:33:54 +0500

    добавил sortabl.js

--- src/components/PackageList.vue
+++ src/components/PackageList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="packege-list">
+    <div id="path_package_list" class="packege-list">
         <div v-for="renderItem of renderList" :key="renderItem?.id" class="packege-list__item">
             <div class="packege-list__item-content" v-if="renderItem.color === cardColor.transparent">
             </div>
@@ -10,8 +10,9 @@
 import { generateUID } from '../utils';
 import type { IListSizeData } from '../package';
 import { cardColor, type cardData } from '../stores/cards'
+import Sortable from 'sortablejs';
 
-import { ref, defineProps } from 'vue';
+import { ref, defineProps, onMounted } from 'vue';
 
 interface IProps {
     listSizeData: IListSizeData,
@@ -49,18 +50,55 @@ for (let i = 0; i < listSize; i++) {
     }
 }
 
+onMounted(() => {
+
+    const flex = document.getElementById('path_package_list');
+    new Sortable(flex, {
+        animation: 0,
+        swap: true,
+        swapThreshold: 0.65,
+        forceFallback: true, // This is it!
+        onChoose: function(e: { target: { classList: { add: (arg0: string) => void; }; }; }) {
+            console.log(e.target)
+            e.target.classList.add('grabbing');
+        },
+        onUnchoose: function(e: { target: { classList: { remove: (arg0: string) => void; }; }; }) {
+            e.target.classList.remove('grabbing');
+        },
+        onStart: function(e: { target: { classList: { add: (arg0: string) => void; }; }; }) {
+            e.target.classList.add('grabbing');
+        },
+        onEnd: function(e: { target: { classList: { remove: (arg0: string) => void; }; }; }) {
+            e.target.classList.remove('grabbing');
+        },
+        onMove: function(e: { target: { classList: { add: (arg0: string) => void; }; }; }) {
+                e.target.classList.add('grabbing');
+            },
+        });
+});
+
 </script>
 <style scoped>
 .packege-list {
     position: absolute;
     display: flex;
     flex-wrap: wrap;
+    margin: 0;
+    padding: 0;
 }
 .packege-list__item {
-    display: flex;
+    display: inline-block;
     justify-content: center;
     align-items: center;
     width: 105px;
     height: 100px;
+}
+.packege-list__item:active {
+    cursor: grabbing!important;
+}
+.grabbing {
+    cursor: grabbing !important;
+    border: 1px solid var(--light_gray);
+    border-radius: var(--br_24);
 }
 </style>
